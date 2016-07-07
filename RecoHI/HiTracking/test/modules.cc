ab3a5ed908726bd017e232a6277eb94c8064060e
erica brondolin <erica.brondolin@cern.ch>
2016-07-07 16:21:10 +0200

    starting point of the seeding algo
    cpe working
    
    cleaning SiPhase2RecHitMatcher

--- RecoHI/HiTracking/test/modules.cc
+++ RecoHI/HiTracking/test/modules.cc
@@ -1,8 +1,15 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoTracker/MeasurementDet/plugins/MeasurementTrackerESProducer.h"
+#include "RecoTracker/MeasurementDet/interface/SeedingOTEDProducer.h"
+
+
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
 
 // Cluster Vertexing
 #include "HIPixelClusterVtxAnalyzer.h"
 DEFINE_FWK_MODULE(HIPixelClusterVtxAnalyzer);
+DEFINE_FWK_EVENTSETUP_MODULE(MeasurementTrackerESProducer);
+DEFINE_FWK_MODULE(SeedingOTEDProducer);
