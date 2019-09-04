0cc021c406c46bba156a1c1308f22cc684ab7244
Jan-Frederik <jschulte@cern.ch>
2019-09-04 21:47:58 +0200

    fix file that prevented compilation when doing a fresh checkout of the branch

--- RecoHI/HiTracking/test/modules.cc
+++ RecoHI/HiTracking/test/modules.cc
@@ -1,13 +1,8 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "RecoTracker/MeasurementDet/plugins/MeasurementTrackerESProducer.h"
-
-
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
 
 // Cluster Vertexing
 #include "HIPixelClusterVtxAnalyzer.h"
 DEFINE_FWK_MODULE(HIPixelClusterVtxAnalyzer);
-DEFINE_FWK_EVENTSETUP_MODULE(MeasurementTrackerESProducer);
